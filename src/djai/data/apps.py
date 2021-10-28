6da28bf5f5cc4f0e7482697920041d4c4df1ebfe
STEAM for Vietnam Foundation: AI & Robotics Educational Initiative <Vinh@STEAMforVietNam.org>
2021-10-27 17:28:09 -0700

    add src.djai.data.apps.DjAIDataModuleConfig

---
+++ src/djai/data/apps.py
@@ -0,0 +1,64 @@
+"""DjAI Data Module Config."""
+
+
+from collections.abc import Sequence
+
+from django.apps.config import AppConfig
+
+
+__all__: Sequence[str] = ('DjAIDataModuleConfig',)
+
+
+# docs.djangoproject.com/en/dev/ref/applications/#application-configuration
+class DjAIDataModuleConfig(AppConfig):
+    """DjAI Data Module Config."""
+
+    # AppConfig.name
+    # Full Python path to the application, e.g. 'django.contrib.admin'.
+    # This attribute defines which application the configuration applies to.
+    # It must be set in all AppConfig subclasses.
+    # It must be unique across a Django project.
+    name: str = 'djai.data'
+
+    # AppConfig.label
+    # Short name for the application, e.g. 'admin'
+    # This attribute allows relabeling an application
+    # when two applications have conflicting labels.
+    # It defaults to the last component of name.
+    # It should be a valid Python identifier.
+    # It must be unique across a Django project.
+    label: str = 'AIData'
+
+    # AppConfig.verbose_name
+    # Human-readable name for the application, e.g. “Administration”.
+    # This attribute defaults to label.title().
+    verbose_name: str = 'DjAI: Data'
+
+    # AppConfig.path
+    # Filesystem path to the application directory,
+    # e.g. '/usr/lib/pythonX.Y/dist-packages/django/contrib/admin'.
+    # In most cases, Django can automatically detect and set this,
+    # but you can also provide an explicit override as a class attribute
+    # on your AppConfig subclass.
+    # In a few situations this is required; for instance
+    # if the app package is a namespace package with multiple paths.
+    # path: str = ...
+
+    # AppConfig.default
+    # Set this attribute to False
+    # to prevent Django from selecting a configuration class automatically.
+    # This is useful when apps.py defines only one AppConfig subclass
+    # but you don’t want Django to use it by default.
+    # Set this attribute to True
+    # to tell Django to select a configuration class automatically.
+    # This is useful when apps.py defines more than one AppConfig subclass
+    # and you want Django to use one of them by default.
+    # By default, this attribute isn’t set.
+    default: bool = True
+
+    # AppConfig.default_auto_field
+    # The implicit primary key type to add to models within this app.
+    # You can use this to keep AutoField
+    # as the primary key type for third party applications.
+    # By default, this is the value of DEFAULT_AUTO_FIELD.
+    default_auto_field: str = 'django.db.models.fields.AutoField'
