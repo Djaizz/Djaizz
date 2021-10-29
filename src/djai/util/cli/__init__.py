3b61f10f4a09d041bd9d55c518976e65f40752cf
STEAM for Vietnam Foundation: AI & Robotics Educational Initiative <Vinh@STEAMforVietNam.org>
2021-10-29 02:08:35 -0700

    update PyProject.TOML packaging files

--- src/djai/util/cli/__init__.py
+++ src/djai/util/cli/__init__.py
@@ -6,20 +6,19 @@ import click
 from .aws_eb import djai_aws_eb
 
 
-@click.group(
-    name='djai',
-    cls=click.Group,
-    commands={'aws-eb': djai_aws_eb},
-    invoke_without_command=False,
-    no_args_is_help=True,
-    subcommand_metavar='DJAI_SUB_COMMAND',
-    chain=False,
-    help='DjAI CLI >>>',
-    epilog='^^^ DjAI CLI',
-    short_help='DjAI CLI',
-    options_metavar='[OPTIONS]',
-    add_help_option=True,
-    hidden=False,
-    deprecated=False)
+@click.group(name='djai',
+             cls=click.Group,
+             commands={'aws-eb': djai_aws_eb},
+             invoke_without_command=False,
+             no_args_is_help=True,
+             subcommand_metavar='DJAI_SUB_COMMAND',
+             chain=False,
+             help='DjAI CLI >>>',
+             epilog='^^^ DjAI CLI',
+             short_help='DjAI CLI',
+             options_metavar='[OPTIONS]',
+             add_help_option=True,
+             hidden=False,
+             deprecated=False)
 def djai():
     """Trigger DjAI from CLI."""
