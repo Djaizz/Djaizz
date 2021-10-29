10607cf9ba49ff640fbb53986c04a408966e9fdc
STEAM for Vietnam Foundation: AI & Robotics Educational Initiative <Vinh@STEAMforVietNam.org>
2021-10-29 00:07:02 -0700

    add src.djai.data.admin

---
+++ src/djai/data/admin.py
@@ -0,0 +1,29 @@
+"""DjAI Data Admin."""
+
+
+from django.contrib.admin.decorators import register
+from django.contrib.admin.options import ModelAdmin
+from django.contrib.admin.sites import site
+
+from silk.profiling.profiler import silk_profile
+
+from djai.data.models import (
+    DataSchema,
+)
+
+
+@register(DataSchema, site=site)
+class DataSchemaAdmin(ModelAdmin):
+    """DataSchema model admin."""
+
+    show_full_result_count = False
+
+    @silk_profile(name=f'{__module__}: {DataSchema._meta.verbose_name}')
+    def changeform_view(self, *args, **kwargs):
+        """Silk-profiled change-form view."""
+        return super().changeform_view(*args, **kwargs)
+
+    @silk_profile(name=f'{__module__}: {DataSchema._meta.verbose_name_plural}')
+    def changelist_view(self, *args, **kwargs):
+        """Silk-profiled change-list view."""
+        return super().changelist_view(*args, **kwargs)
