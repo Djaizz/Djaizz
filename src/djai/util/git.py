7ac11ea7bff7f437ad71901833cb96260eb57813
STEAM for Vietnam Foundation: AI & Robotics Educational Initiative <Vinh@STEAMforVietNam.org>
2021-10-29 01:34:59 -0700

    add src.djai.util.git

---
+++ src/djai/util/git.py
@@ -0,0 +1,37 @@
+"""DjAI Git-related Utilities."""
+
+
+from collections.abc import Sequence
+from pathlib import Path
+from typing import Optional
+
+from git.exc import InvalidGitRepositoryError
+from git.repo.base import Repo
+
+
+__all__: Sequence[str] = ('get_git_repo_head_commit_hash',)
+
+
+_GIT_HASH_FILE_NAME: str = '.git-hash'
+
+
+def get_git_repo_head_commit_hash(path: Optional[str] = None) -> str:
+    """Get Git Repo Head Commit Hash."""
+    try:
+        repo: Repo = Repo(path=path,
+                          search_parent_directories=True,
+                          expand_vars=True)
+
+        return repo.head.commit.hexsha
+
+    except InvalidGitRepositoryError:
+        if Path(_GIT_HASH_FILE_NAME).is_file():
+            with open(file=_GIT_HASH_FILE_NAME,
+                      mode='rt',
+                      buffering=-1,
+                      encoding='utf-8',
+                      errors='strict',
+                      newline=None,
+                      closefd=True,
+                      opener=None) as f:
+                return f.read()
