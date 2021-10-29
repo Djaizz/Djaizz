1419ae8680f52c468d4b85edc315a70646c817d0
STEAM for Vietnam Foundation: AI & Robotics Educational Initiative <Vinh@STEAMforVietNam.org>
2021-10-28 20:52:17 -0700

    add src.djai.util.cli._server_files

---
+++ src/djai/util/cli/_server_files/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for DjAI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/asgi
+"""
+
+
+from django.core.asgi import get_asgi_application
+import os
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
+
+application = get_asgi_application()
