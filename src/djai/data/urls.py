db4c28e726b04be610ab6c11b80e073280194cb2
STEAM for Vietnam Foundation: AI & Robotics Educational Initiative <Vinh@STEAMforVietNam.org>
2021-10-29 00:09:53 -0700

    add skeletal src.djai.data.urls

---
+++ src/djai/data/urls.py
@@ -0,0 +1,13 @@
+"""DjAI Data URLs."""
+
+
+from collections.abc import Sequence
+
+from django.urls.resolvers import URLPattern
+
+
+__all__: Sequence[str] = ('urlpatterns',)
+
+
+urlpatterns: Sequence[URLPattern] = (
+)
--- src/djai/urls.py
+++ src/djai/urls.py
@@ -6,6 +6,7 @@ from collections.abc import Sequence
 from django.urls.conf import include, path
 from django.urls.resolvers import URLPattern
 
+from .data import urls as data_urls
 from .model import urls as model_urls
 
 
@@ -13,6 +14,11 @@ __all__: Sequence[str] = ('urlpatterns',)
 
 
 urlpatterns: Sequence[URLPattern] = (
+    path(route='data/',
+         view=include(data_urls), kwargs=None, name=None),
+    path(route='data-mgmt/',
+         view=include(data_urls), kwargs=None, name=None),
+
     path(route='model/',
          view=include(model_urls), kwargs=None, name=None),
     path(route='models/',
