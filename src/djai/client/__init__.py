e8420ee02fe52ec843e60063dc35e198c51c8bae
STEAM for Vietnam Foundation: AI & Robotics Educational Initiative <Vinh@STEAMforVietNam.org>
2021-12-08 22:08:29 -0800

    add place-holder src.djai.client.DjAIClient

---
+++ src/djai/client/__init__.py
@@ -0,0 +1,8 @@
+"""Python client to access DjAI data & models."""
+
+
+class DjAIClient:   # pylint: disable=too-few-public-methods
+    """Python client to access DjAI data & models."""
+
+    def get_prediction(self, *args, **kwargs):
+        """Get prediction from DjAI server."""
